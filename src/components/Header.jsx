82a2cc6899175c0d33c5860a5302cc48fee86e36
Svilen Grandzhan <peructep@gmail.com>
2023-12-26 16:08:29 -0700

    End day commit 20231226

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useContext } from "react"
 
-import CartModal from './CartModal.jsx';
+import CartModal from "./CartModal.jsx"
+import { ShoppingCartContext } from "../store/shopping-cart-context.jsx"
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
-  const modal = useRef();
+export default function Header() {
+  const modal = useRef()
+  const { items } = useContext(ShoppingCartContext)
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length
 
   function handleOpenCartClick() {
-    modal.current.open();
+    modal.current.open()
   }
 
-  let modalActions = <button>Close</button>;
+  let modalActions = <button>Close</button>
 
   if (cartQuantity > 0) {
     modalActions = (
@@ -19,21 +21,22 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
         <button>Close</button>
         <button>Checkout</button>
       </>
-    );
+    )
   }
 
   return (
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
       <header id="main-header">
         <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
+          <img
+            src="logo.png"
+            alt="Elegant model"
+          />
           <h1>Elegant Context</h1>
         </div>
         <p>
@@ -41,5 +44,5 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
         </p>
       </header>
     </>
-  );
+  )
 }
